38604a6b8972bad49b2fb1b77a88e77243d8a925
huuyafwww <huuya1234fwww@gmail.com>
2019-11-04 10:13:28 +0900

    Add the file Helper Proccess

--- system/helper/__file.php
+++ system/helper/__file.php
@@ -14,21 +14,54 @@ function __save_file($file,$data,$append = true){
 }
 
 /**
+ * Get File List
+ *
+ * @param string $dir
+ * @param string $extension
+ * @return array
+ */
+function __get_file_list($dir,$extension = "*"){
+    return glob(rtrim($dir,"/")."/".$extension);
+}
+
+/**
  * Get ALL FILE PATH from $dir
  *
  * @param string $dir
  * @return array
  */
-function __get_file_list($dir){
-    $files = glob(rtrim($dir,'/').'/*');
+function __get_all_file_list($dir,$extension = "*"){
+    $files = __get_file_list($dir,$extension);
     $list = [];
     foreach($files as $file){
         if(is_file($file)){
             $list[] = $file;
         }
         if(is_dir($file)){
-            $list = array_merge($list, __get_file_list($file));
+            $list = array_merge($list, __get_all_file_list($file));
         }
     }
     return $list;
+}
+
+/**
+ * Delete Files
+ *
+ * @param string|array $dirs
+ * @param string $extension
+ */
+function __delete_files($dirs,$extension = "*"){
+    if(!is_array($dirs)){
+        $files = __get_file_list($dirs,$extension);
+        foreach($files as $file){
+            unlink($file);
+        }
+    }else{
+        foreach($dirs as $dir){
+            $files = __get_file_list($dir,$extension);
+            foreach($files as $file){
+                unlink($file);
+            }
+        }
+    }
 }
